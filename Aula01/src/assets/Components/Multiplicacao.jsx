a40e2479a19930a213c412499d4793a2686a63ba
Zdornelas <samueldornelas.pb@gmail.com>
2024-08-27 22:59:29 -0300

    first commit

---
+++ Aula01/src/assets/Components/Multiplicacao.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Multiplicacao = (props) => {
+  return (
+    <div>
+      O resultado de num1 * num2 é igual a:
+      {props.num1*props.num2}
+    </div>
+  )
+}
+
+export default Multiplicacao
